ea26335ea445b98a9d25966c45188718a33aeb2e
azurata <azurata@vivaldi.net>
2019-04-03 12:45:17 +0900

    Docコメントの追加

--- src/main/java/jp/dip/azurata/jinromc/player/Werewolf.java
+++ src/main/java/jp/dip/azurata/jinromc/player/Werewolf.java
@@ -4,19 +4,27 @@ import org.bukkit.entity.Player;
 
 public final class Werewolf extends JinroPlayer {
 
-	private Player attack;
+	private Player attackee;
 
 	public Werewolf(Player player) {
 		super(player);
 		super.role = JinroRole.werewolf;
 	}
 
+	/**
+	 * 夜のターンの攻撃先を取得
+	 * @return 攻撃先
+	 */
 	public Player getAttack() {
-		return this.attack;
+		return this.attackee;
 	}
 
-	public void setAttack(Player attack) {
-		this.attack = attack;
+	/**
+	 * 夜のターンの攻撃先を設定
+	 * @param attackee 攻撃先
+	 */
+	public void setAttack(Player attackee) {
+		this.attackee = attackee;
 	}
 
 }
