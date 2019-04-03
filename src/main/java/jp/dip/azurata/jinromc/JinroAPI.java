36e7fa0d4c109ed10a2e1bdf0d47bcdf8ec7d01c
azurata <azurata@vivaldi.net>
2019-04-03 13:17:59 +0900

    Docコメントの追加, 使用しないメソッドを一つ削除

--- src/main/java/jp/dip/azurata/jinromc/JinroAPI.java
+++ src/main/java/jp/dip/azurata/jinromc/JinroAPI.java
@@ -6,7 +6,7 @@ import jp.dip.azurata.jinromc.player.JinroPlayer;
 
 public final class JinroAPI {
 
-	public void next() {
+	public void nextTurn() {
 		switch(JinroMC.time) {
 		case Before:
 			JinroMC.log("現在はゲーム前のため、次の時間に進めません");
@@ -20,6 +20,11 @@ public final class JinroAPI {
 		}
 	}
 
+	/**
+	 * 引数で指定されたプレイヤーと紐づけたれたJinroPlayer型のオブジェクトを返す
+	 * @param player プレイヤーを指定
+	 * @return playerを持つJinroPlayer型のオブジェクト。プレイヤーリストから見つからない場合はnullを返す
+	 */
 	public JinroPlayer getJinroPlayer(Player player) {
 		for(JinroPlayer jinroPlayer : JinroMC.players) {
 			if(jinroPlayer.getPlayer().equals(player)) {
@@ -29,11 +34,4 @@ public final class JinroAPI {
 		return null;
 	}
 
-	public void judgeGoToNextTurn() {
-		for(JinroPlayer player : JinroMC.players) {
-			if(!player.isTurndEnd())
-				return;
-		}
-		next();
-	}
 }
